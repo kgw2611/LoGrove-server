ec5acd528724c1b1877aade60240c1cc079075e9
2171147 <kenyouzzang@gmail.com>
2026-03-24 17:30:42 +0900

    Initialize

---
+++ src/test/java/com/hansung/logrove/LogroveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansung.logrove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogroveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
